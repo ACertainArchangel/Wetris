3384c48201d96d22307b6c17215f46478219d790
ACertainArchangel <gabrielvanrijnjordaan@gmail.com>
2025-12-09 17:37:27 -0800

    Delete old code

--- lib/WaterGun/WaterGun.cpp
+++ lib/WaterGun/WaterGun.cpp
@@ -1,42 +1,4 @@
 #include "WaterGun.h"
-#include <Arduino.h>
-
-namespace WaterGun {
-
-    const int FWD_PIN = 22;
-    const int BACK_PIN = 23;
-    bool shot = false;
-
-    void setup() {
-        pinMode(FWD_PIN, OUTPUT);
-        pinMode(BACK_PIN, OUTPUT);
-        digitalWrite(FWD_PIN, LOW);
-        digitalWrite(BACK_PIN, HIGH);
-    }
-
-    bool shoot() {
-        if (!shot) {
-
-            //Make doubly sure both are low in case of some bizzare hardware bug or problem with someone else's code.
-            //This is to prevent a short.
-
-            digitalWrite(BACK_PIN, LOW);
-            digitalWrite(FWD_PIN, LOW);
-
-            int startTime = millis(); while (millis() - startTime < 500) {};
-
-            digitalWrite(FWD_PIN, HIGH);
-            shot = true;
-        }
-        return shot;
-    }
-
-}
-
-// Hey Gabe, lmk if this is what you are looking for.
-// If it is, then we can delete the above code. 
-
-#include "WaterGun.h"
 #include <Arduino.h>  
 
 namespace WaterGun {
